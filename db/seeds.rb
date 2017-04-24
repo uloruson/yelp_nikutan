09050a12daeea4f1a741ff512a5193e4e8417386
MMM <mmm@server.fake>
2017-04-24 23:21:39 +0900

    Renew seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 require "csv"
 
-CSV.foreach('db/postal.csv', headers: true, row_sep: "\r\n", encoding: "SJIS") do |row|
-  Postalcode.create(code: row[0], city: row[1], address: row[2])
+CSV.open('db/postal.csv', :row_sep => :auto, :col_sep => ";" , encoding: 'Shift_JIS:UTF-8') do |csv|
+    csv.each { |row| Postalcode.create(code: row[0], city: row[1], address: row[2]) }
 end
