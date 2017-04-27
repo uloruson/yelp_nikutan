b34a7ef69f8b87473a2fcffcd70c59d636013b9a
MMM <mmm@server.fake>
2017-04-27 20:48:38 +0900

    seed Correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 require "csv"
 
-CSV.open('db/postal.csv', :row_sep => :auto, :col_sep => ";" , encoding: 'Shift_JIS:UTF-8') do |csv|
+CSV.open('db/postal.csv', :row_sep => :auto, :col_sep => "," , encoding: 'Shift_JIS:UTF-8') do |csv|
     csv.each { |row| Postalcode.create(code: row[0], city: row[1], address: row[2]) }
 end
